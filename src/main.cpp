65bad168bc448158b3467f9400fd84904c268caf
LengDom <jamielendon86@gmail.com>
2025-12-18 01:26:36 +0000

    Lab01

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,24 @@
 
 void initTriangle()
 {
-    // TODO: add triangle vertex data
-
-    // TODO: create vertex buffer
-
-    // TODO: set buffer data to triangle vertex and setting vertex attributes
-
+    GLfloat verts[] = {
+    -1.0f, -1.0f,
+    1.0f, -1.0f,
+    0.0f, 1.0f,
+};
+    GLuint bufID;
+    glGenBuffers(1, &bufID);
+    glBindBuffer(GL_ARRAY_BUFFER, bufID);
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 }
 
-// TODO: draw triangle
 void drawTriangle()
 {
-
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main()
@@ -32,7 +38,7 @@ int main()
     window = glfwCreateWindow(640, 480, "Hello OpenGL1", NULL, NULL);
     glfwMakeContextCurrent(window);
 
-    // loading glad
+       // loading glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Couuldn't load opengl" << std::endl;
@@ -40,18 +46,19 @@ int main()
         return -1;
     }
 
-    // TODO: call initTriangle()
+    initTriangle();
 
-
-    // TODO: set the background colour using glClearColor(r, g, b, a)
-
+    // setting the background colour, you can change the value
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
 
     // setting the event loop
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
-        // TODO : clear the background colour and call drawTriangle()
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        drawTriangle();
 
         glfwSwapBuffers(window);
     }
